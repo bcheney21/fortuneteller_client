cf739c6b7d0b70ba266a4062237d7b59a0ec43a9
NBryant83 <nakisha.bryant@gmail.com>
2021-03-23 12:22:00 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Navbar from './components/partials/Navbar.jsx'
+import Login from './components/Login.jsx'
+import Logout from './components/Logout.jsx'
+import Profile from './components/Profile.jsx'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  return(
+  <Router>
+     <div>
+       <Navbar />
+       <Profile />
+       <Login />
+       <Logout />
+     </div>
+   </Router>
+  // <div>Hello World</div>
+  )
+ 
 }
 
 export default App;
