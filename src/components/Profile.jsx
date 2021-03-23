97bf2f283421419c1324744108522ac3cfb6af53
NBryant83 <nakisha.bryant@gmail.com>
2021-03-23 18:42:12 -0400

    changed some things in app.js

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,10 @@
+import { useState, useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+import Login from './Login'
+
 export default function Profile(props) {
+
   return(
     <div>
       hello from profile!
