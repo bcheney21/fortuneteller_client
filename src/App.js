e51479eaa782be421e18b24bcd1f9943dd54f77d
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-23 16:17:28 -0700

    added imports to profile.jsx

--- src/App.js
+++ src/App.js
@@ -1,57 +1,52 @@
-
-  
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom'
-import jwt_decode from 'jwt-decode'
-import Navbar from './components/partials/Navbar.jsx'
-import Login from './components/Login.jsx'
-import Logout from './components/Logout.jsx'
-import Profile from './components/Profile.jsx'
-import Register from './components/Register.jsx'
-import Welcome from './components/Welcome.jsx'
-import './App.css';
-
-
-
-
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import Navbar from "./components/partials/Navbar.jsx";
+import Login from "./components/Login.jsx";
+import Logout from "./components/Logout.jsx";
+import Profile from "./components/Profile.jsx";
+import Register from "./components/Register.jsx";
+import Welcome from "./components/Welcome.jsx";
+import "./App.css";
 
 const App = () => {
-
-  const [currentUser, setCurrentUser] = useState(null)
+  const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
-    const token = localStorage.getItem('jwtToken')
-    if(token) {
-      const decoded = jwt_decode(token)
-      setCurrentUser(decoded)
+    const token = localStorage.getItem("jwtToken");
+    if (token) {
+      const decoded = jwt_decode(token);
+      setCurrentUser(decoded);
     } else {
-      setCurrentUser(null)
+      setCurrentUser(null);
     }
-  }, [])
+  }, []);
 
   const handleLogout = () => {
-    if(localStorage.getItem('jwtToken')) {
-      localStorage.removeItem('jwtToken')
-      setCurrentUser(null)
+    if (localStorage.getItem("jwtToken")) {
+      localStorage.removeItem("jwtToken");
+      setCurrentUser(null);
     }
-  }
+  };
 
-
-
-  return(
-  <Router>
-     <div>       
-       <Navbar />
-       <Profile />
-       <Login />
-       <Logout />
-       <Register />
-     </div>
-   </Router>
-  // <div>Hello World</div>
-  )
- 
-}
-
+  return (
+    <Router>
+      <div className="main-div">
+        <Navbar className="navbar-pg" />
+        <Profile className="profile-pg" />
+        <Login className="login-pg" />
+        <Logout className="logout-pg" />
+        <Register className="register-pg" />
+      </div>
+    </Router>
+    // <div>Hello World</div>
+  );
+};
 
 export default App;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,8 @@
+import { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
+import Login from "./Login";
+
 export default function Profile(props) {
   return <div>hello from profile!</div>;
 }
