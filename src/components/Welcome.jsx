6690f570f443511f7c08c39e001404700563c89e
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-24 11:45:38 -0700

    added css styling and button into crystal ball

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,7 @@
-
-
-
 export default function Welcome(props) {
-  return(
-    <div>
+  return (
+    <div className="welcome">
       <h4>💫 Welcome to Madame's Parlor of Fortune! 💫</h4>
     </div>
-  )
+  );
 }
