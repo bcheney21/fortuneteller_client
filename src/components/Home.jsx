4d198d087a6312e2e4db9a2e2444a765011bba34
NBryant83 <nakisha.bryant@gmail.com>
2021-03-25 12:25:30 -0400

    added <register /> to home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 //IMPORTS//
+import Register from './Register'
 import madame from "../public/hands.gif";
 
 export default function Home(props) {
@@ -6,6 +7,7 @@ export default function Home(props) {
     <div className="home">
       <h4 className="opening-banner">Madame's Parlor of Fortune</h4>
       <img src={madame} className="hands-img" />
+      <Register />
     </div>
   );
 }
