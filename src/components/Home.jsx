a912044bc2ff5a60e4727dac1797797f3c394a01
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-26 10:26:07 -0700

    fixed nav bar, and idx keys

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 //IMPORTS//
-import Register from "./Register";
+// import Register from "./Register";
 import madame from "../public/hands.gif";
 
 export default function Home(props) {
   return (
     <div className="home">
       <h4 className="opening-banner">Madame's Parlor of Fortune</h4>
-      <Register />
+      {/* <Register /> */}
       <img src={madame} className="hands-img" />
     </div>
   );
