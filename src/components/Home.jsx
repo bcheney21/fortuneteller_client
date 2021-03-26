6c277508bd124636f60b601a50b6705000d17055
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-26 11:33:57 -0700

    added flickering neon text for welcome & crystal ball

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,10 @@ import madame from "../public/hands.gif";
 export default function Home(props) {
   return (
     <div className="home">
-      <h4 className="opening-banner">Madame's Parlor of Fortune</h4>
-      {/* <Register /> */}
+      <h4 className="opening-banner">
+        Madame's Parlor
+        <br /> of Fortune
+      </h4>
       <img src={madame} className="hands-img" />
     </div>
   );
